3cb885b4f0cb4ba2890df77626fd0b2d3a88fe74
pinhaskonde <pinhas.konde@gmail.com>
2022-11-09 17:07:40 +0200

    commit 1

---
+++ src/main/java/BaseHelper.java
@@ -0,0 +1,15 @@
+import ch.qos.logback.classic.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.LoggerFactory;
+
+public class BaseHelper {
+
+
+    WebDriver wd = new ChromeDriver();
+
+
+
+
+
+}
--- src/test/java/NewTestClass.java
+++ src/test/java/NewTestClass.java
@@ -1,2 +1,31 @@
-public class NewTestClass {
+import ch.qos.logback.classic.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.Test;
+
+public class NewTestClass extends BaseHelper {
+
+    Logger logger = (Logger) LoggerFactory.getLogger(NewTestClass.class);
+
+
+    @Test
+    public void firstTest() {
+        logger.info("Start logging");
+
+        wd.get("http://students-aid.org:9222/");
+        wd.manage().window().maximize();
+
+//        WebElement registerButton = wd.findElement(By.xpath("//button[@id='register']"));
+//        registerButton.click();
+
+//        WebElement firstNameInput = wd.findElement(By.xpath("//input[@id='firstname']"));
+//        firstNameInput.clear();
+
+        wd.quit();
+
+
+    }
+
+
 }
